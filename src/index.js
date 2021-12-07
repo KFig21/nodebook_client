f2748afddee3941c18e602679381aea5060bef14
Kevin <kfig21@gmail.com>
2021-12-07 17:35:02 -0500

    trying to fix gh pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AuthContextProvider } from "./context/AuthContext";
-import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <Router basename="/">
-        <App />
-      </Router>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
