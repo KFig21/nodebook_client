fb3ed9b9192f3ad75994985d97cadfa6a203ba4f
Kevin <kfig21@gmail.com>
2021-12-08 13:50:51 -0500

    sidebar footer

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -14,9 +14,11 @@ import {
   GroupOutlined,
   Language,
   Public,
+  GitHub,
 } from "@material-ui/icons";
 import { logoutCall } from "../../apiCalls";
 import Logo from "./Logo";
+import KF from "./KF";
 
 export default function Sidebar({
   currentPage,
@@ -39,126 +41,145 @@ export default function Sidebar({
   };
 
   return (
-    <div className={"sidebar " + (sidebarOpen && "active")}>
+    <div className={sidebarOpen ? "sidebar active" : "sidebar"}>
       <div className="sidebar-wrapper">
         <div className="sidebar-logo-container">
           <Logo />
         </div>
         {/* TIMELINE */}
-        <Link to="/">
-          <div
-            onClick={() => handleOptionClick()}
-            className={
-              "sidebar-link " +
-              (currentPage === "Timeline" ? "active-nav-link" : "")
-            }
-          >
-            {currentPage === "Timeline" ? (
-              <HomeRounded className="sidebar-icon" />
-            ) : (
-              <HomeOutlined className="sidebar-icon" />
-            )}{" "}
-            Timeline
-          </div>
-        </Link>
-        {/* PROFILE */}
-        <Link to={"/profile/" + user.username}>
-          <div
-            onClick={() => handleOptionClick()}
-            className={
-              "sidebar-link " +
-              (currentPage === "Profile" ? "active-nav-link" : "")
-            }
-          >
-            {currentPage === "Profile" ? (
-              <Person className="sidebar-icon" />
-            ) : (
-              <PersonOutlineRounded className="sidebar-icon" />
-            )}{" "}
-            Profile
-          </div>
-        </Link>
-        {/* NOTIFICATIONS */}
-        <Link to={"/notifications/" + user.username}>
-          <div
-            onClick={() => handleOptionClick()}
-            className={
-              "sidebar-link " +
-              (currentPage === "Notifications" ? "active-nav-link" : "")
-            }
-          >
-            {currentPage === "Notifications" ? (
-              <Notifications className="sidebar-icon" />
-            ) : (
-              <NotificationsOutlined className="sidebar-icon" />
-            )}{" "}
-            Notifications
-            {notificationsCount > 0 && (
-              <div className="notification-badge-container">
-                <div className="notification-badge">
-                  <span className="badge-count">{notificationsCount}</span>
+        <>
+          <Link to="/">
+            <div
+              onClick={() => handleOptionClick()}
+              className={
+                "sidebar-link " +
+                (currentPage === "Timeline" ? "active-nav-link" : "")
+              }
+            >
+              {currentPage === "Timeline" ? (
+                <HomeRounded className="sidebar-icon" />
+              ) : (
+                <HomeOutlined className="sidebar-icon" />
+              )}{" "}
+              Timeline
+            </div>
+          </Link>
+          {/* PROFILE */}
+          <Link to={"/profile/" + user.username}>
+            <div
+              onClick={() => handleOptionClick()}
+              className={
+                "sidebar-link " +
+                (currentPage === "Profile" ? "active-nav-link" : "")
+              }
+            >
+              {currentPage === "Profile" ? (
+                <Person className="sidebar-icon" />
+              ) : (
+                <PersonOutlineRounded className="sidebar-icon" />
+              )}{" "}
+              Profile
+            </div>
+          </Link>
+          {/* NOTIFICATIONS */}
+          <Link to={"/notifications/" + user.username}>
+            <div
+              onClick={() => handleOptionClick()}
+              className={
+                "sidebar-link " +
+                (currentPage === "Notifications" ? "active-nav-link" : "")
+              }
+            >
+              {currentPage === "Notifications" ? (
+                <Notifications className="sidebar-icon" />
+              ) : (
+                <NotificationsOutlined className="sidebar-icon" />
+              )}{" "}
+              Notifications
+              {notificationsCount > 0 && (
+                <div className="notification-badge-container">
+                  <div className="notification-badge">
+                    <span className="badge-count">{notificationsCount}</span>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
+          </Link>
+          {/* FOLLOWERS */}
+          <Link to={`/${user.username}/followers`}>
+            <div
+              onClick={() => handleOptionClick()}
+              className={
+                "sidebar-link " +
+                (currentPage === "Followers" ? "active-nav-link" : "")
+              }
+            >
+              {currentPage === "Followers" ? (
+                <Group className="sidebar-icon" />
+              ) : (
+                <GroupOutlined className="sidebar-icon" />
+              )}{" "}
+              Followers
+            </div>
+          </Link>
+          {/* FOLLOWING */}
+          <Link to={`/${user.username}/following`}>
+            <div
+              onClick={() => handleOptionClick()}
+              className={
+                "sidebar-link " +
+                (currentPage === "Following" ? "active-nav-link" : "")
+              }
+            >
+              {currentPage === "Following" ? (
+                <Group className="sidebar-icon" />
+              ) : (
+                <GroupOutlined className="sidebar-icon" />
+              )}{" "}
+              Following
+            </div>
+          </Link>
+          {/* EXPLORE */}
+          <Link to={`/explore/${user._id}`}>
+            <div
+              onClick={() => handleOptionClick()}
+              className={
+                "sidebar-link " +
+                (currentPage === "Explore" ? "active-nav-link" : "")
+              }
+            >
+              {currentPage === "Explore" ? (
+                <Public className="sidebar-icon" />
+              ) : (
+                <Language className="sidebar-icon" />
+              )}{" "}
+              Explore
+            </div>
+          </Link>
+          {/* LOGOUT */}
+          <div className="sidebar-logout">
+            <button className="logout-button" onClick={logout}>
+              <ExitToApp className="sidebar-icon" /> Logout
+            </button>
           </div>
-        </Link>
-        {/* FOLLOWERS */}
-        <Link to={`/${user.username}/followers`}>
-          <div
-            onClick={() => handleOptionClick()}
-            className={
-              "sidebar-link " +
-              (currentPage === "Followers" ? "active-nav-link" : "")
-            }
-          >
-            {currentPage === "Followers" ? (
-              <Group className="sidebar-icon" />
-            ) : (
-              <GroupOutlined className="sidebar-icon" />
-            )}{" "}
-            Followers
-          </div>
-        </Link>
-        {/* FOLLOWING */}
-        <Link to={`/${user.username}/following`}>
-          <div
-            onClick={() => handleOptionClick()}
-            className={
-              "sidebar-link " +
-              (currentPage === "Following" ? "active-nav-link" : "")
-            }
-          >
-            {currentPage === "Following" ? (
-              <Group className="sidebar-icon" />
-            ) : (
-              <GroupOutlined className="sidebar-icon" />
-            )}{" "}
-            Following
-          </div>
-        </Link>
-        {/* EXPLORE */}
-        <Link to={`/explore/${user._id}`}>
-          <div
-            onClick={() => handleOptionClick()}
-            className={
-              "sidebar-link " +
-              (currentPage === "Explore" ? "active-nav-link" : "")
-            }
-          >
-            {currentPage === "Explore" ? (
-              <Public className="sidebar-icon" />
-            ) : (
-              <Language className="sidebar-icon" />
-            )}{" "}
-            Explore
-          </div>
-        </Link>
-        {/* LOGOUT */}
-        <div className="sidebar-logout">
-          <button className="logout-button" onClick={logout}>
-            <ExitToApp className="sidebar-icon" /> Logout
-          </button>
-        </div>
+        </>
+      </div>
+      <div className="sidebar-personal-info-container">
+        <a
+          href="https://kfig21.github.io/portfolio_2021/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <KF />
+        </a>
+        <span className="made-by">Made by KFig21</span>
+        <a
+          href="https://github.com/KFig21/nodebook_client"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub className="sidebar-icon" />
+        </a>
       </div>
     </div>
   );
